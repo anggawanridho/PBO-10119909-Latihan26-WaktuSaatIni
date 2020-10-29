133b956a37a94fe0ec3ad53c02ac8ec1de65d0df
anggawanridho <55708748+anggawanridho@users.noreply.github.com>
2020-10-29 22:41:31 +0700

    Update PBO10119909Latihan26.java

--- src/waktu_saat_ini/PBO10119909Latihan26.java
+++ src/waktu_saat_ini/PBO10119909Latihan26.java
@@ -1,10 +1,8 @@
 package waktu_saat_ini;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-import java.util.*;
-import java.time.format.DateTimeFormatter;
-import java.time.LocalDateTime;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 
 /**
  *
@@ -18,16 +16,16 @@ public class PBO10119909Latihan26 {
 
     public static void main(String[] args) {
         int detik, menit, jam, hari, tanggal, bulan, tahun;
-        GregorianCalendar date = new GregorianCalendar();
+        GregorianCalendar waktu = new GregorianCalendar();
         String namabulan[] = {"Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"};
-        String nama_hari[] = {"Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"};
-        detik = date.get(Calendar.SECOND);
-        menit = date.get(Calendar.MINUTE);
-        jam =  date.get(Calendar.HOUR_OF_DAY);
-        hari = date.get(Calendar.DAY_OF_WEEK);
-        tanggal = date.get(Calendar.DAY_OF_MONTH);
-        bulan = date.get(Calendar.MONTH);
-        tahun = date.get(Calendar.YEAR);
+        String nama_hari[] = {"Sabtu", "Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat"};
+        detik = waktu.get(Calendar.SECOND);
+        menit = waktu.get(Calendar.MINUTE);
+        jam =  waktu.get(Calendar.HOUR_OF_DAY);
+        hari = waktu.get(Calendar.DAY_OF_WEEK);
+        tanggal = waktu.get(Calendar.DAY_OF_MONTH);
+        bulan = waktu.get(Calendar.MONTH);
+        tahun = waktu.get(Calendar.YEAR);
         System.out.println("Hari ini adalah hari : "+nama_hari[hari]+", "+tanggal+" "+namabulan[bulan]+" "+tahun+" "+jam+":"+menit+":"+detik);
 
     }
